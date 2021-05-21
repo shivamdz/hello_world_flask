7baeb11e46d80ebd3bb5fdc8fbe92daeffb431fc
Shivam Pandey I341903 <shivam.pandey@sap.com>
2021-05-21 11:26:13 +0530

    updated flask run

--- hello.py
+++ hello.py
@@ -3,14 +3,10 @@ import os
 
 app = Flask(__name__)
 
-cf_port = os.getenv('PORT')
-
 @app.route('/')
 def hello():
     return 'Hello World'
 
 if __name__ == '__main__':
-	if cf_port is None:
-		app.run(host='0.0.0.0', port=5000, debug=True)
-	else:
-		app.run(host='0.0.0.0', port=int(cf_port))
+	app.run()
+		
