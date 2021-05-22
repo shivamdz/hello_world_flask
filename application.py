ce0dc2a70087868d1c8a1f498bac9d1850cb6ec8
shivamdz <11841070+shivamdz@users.noreply.github.com>
2021-05-22 09:40:30 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,17 @@
 
 from flask import Flask
 
-# print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda:
     say_hello()))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
 application.add_url_rule('/<username>', 'hello', (lambda username:
     say_hello(username)))
 
-# run the app.
 if __name__ == "__main__":
     application.debug = True
     application.run()
